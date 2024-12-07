5724ef2623d2c717955bc0cda0898c0647c05d02
unknown <kartikpatel1323@gmail.com>
2024-12-07 13:29:54 +0530

    Initial commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD29w0YWOmuL8-3iT___wpW7-_F8ve_25E",
+  authDomain: "reactecommerce-8986b.firebaseapp.com",
+  projectId: "reactecommerce-8986b",
+  storageBucket: "reactecommerce-8986b.appspot.com",
+  messagingSenderId: "808133317081",
+  appId: "1:808133317081:web:61500566ac099dd6ff09c8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
